f2c32d2f34f3d2f17553dfb49670039bb491592e
M7MD3AMD <medo_emad2011@yahoo.com>
2019-04-23 23:00:13 +0200

    new1

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'survivors'),
+            'username' => env('DB_USERNAME', 'm7md3mad'),
+            'password' => env('DB_PASSWORD', 'Medo321546'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
